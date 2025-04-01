0d62293928ac0c0ba5ef3d6c7289c4fc18489c54
Hydro789 <147501930+Hydro789@users.noreply.github.com>
2025-04-01 12:16:36 +0100

    Camera Movement
    Added camera movement to the game so the audience will be able to explore the blockout

---
+++ 2D Side scrolling Japanese Game/Assets/CameraMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private float zoomStep, minCamSize, maxCamSize;
+
+    private Vector3 dragOrigin;
+
+
+
+    private void Update()
+    {
+        PanCamera();
+    }
+
+    private void PanCamera()
+    {
+        if(Input.GetMouseButtonDown(0))
+        dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            // print("origin" + dragOrigin + "newPosition" + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference " + difference);
+            
+            cam.transform.position +=difference;
+        }
+    }
+    
+    public void ZoomIn()
+    {
+        float newSize = cam.orthographicSize - zoomStep;
+        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+    }
+        public void ZoomOut()
+    {
+        float newSize = cam.orthographicSize + zoomStep;
+        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+    }
+}
